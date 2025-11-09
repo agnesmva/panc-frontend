2f74c0d46eaabe40aedc0110f669cb37d6e5805b
agnesmva <agnesmorganav@gmail.com>
2025-11-09 02:40:55 -0300

    main dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/footer/footer'
+
+
+function MainDashboard() {
+  return (
+    <div>
+      <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col min-h-screen py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h2>Página em Construção</h2>
+      </main>
+    </div>
+    </div>
+  )
+}
+
+export default MainDashboard
